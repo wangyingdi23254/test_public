179b90855360bce7e25a6291b2de621a3c06240c
WYD <wyd@tsinghua.org.cn>
2020-11-12 15:21:23 +0800

    modify

--- matlab_script/imagecrop.m
+++ matlab_script/imagecrop.m
@@ -13,7 +13,7 @@ for i=1:imnum
     imname = imfiles(i).name;
     im = imread([path '\' imname]);
     %[x y]=size(im);
-    im1 = imcrop(im,[0,0,2072,630]); % crop image by rectangle [x1,y1,x2,y2]
+    im1 = imcrop(im,[0,0,1072,630]); % crop image by rectangle [x1,y1,x2,y2]
     imwrite(uint8(im1),[dirname1, '\', num2str(i,'%03d'),'.tif'],'tif')
     im2 = imcrop(im,[0,630,2072,1260]);
     imwrite(uint8(im2),[dirname2, '\', num2str(i,'%03d'),'.tif'],'tif')
